4a77360d4d029748e2e5767ec0a7410f71565427
Douglas Craig <dcraig112@gmail.com>
2018-05-14 22:05:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-
 function myAnimal() {
   var name = "dog"
   return name
 }
+
 function yourAnimal() {
   let yours = 'cat'
   return yours
 }
+
 function add2(n) {
   let r = n + 2
   return r
 }
+
 function funkyFunction() {
-  return myAnimal
+  return theFunk
 }
 
 var theFunk = 'FUNKY!'
